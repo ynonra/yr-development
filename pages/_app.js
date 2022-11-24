adc71c336d67ebc70fa2f59ef06de66e1862b5ba
Yinon Rahamim <ynonra@gmail.com>
2022-11-24 13:08:33 +0200

    next Audiences  instead of google analitics

--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,12 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 import Layout from "../components/layout/Layout";
-
-import * as gtag from '../lib/gtag'
-
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    router.events.on("hashChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-      router.events.off("hashChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <Layout>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
